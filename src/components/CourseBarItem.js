2afaaa8b5bdcd21ffb42b694b631f24aa27a1ea6
Branden Lee <brandenlee@Brandens-MacBook-Pro.local>
2019-09-29 14:05:47 -0400

    finally moved search bar to the middle

--- src/components/CourseBarItem.js
+++ src/components/CourseBarItem.js
@@ -5,26 +5,24 @@ const CourseBarItem = ({titleChanged, searchText}) =>
 
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
     <a className="navbar-brand" href="#"> <Dropdown/> </a>
-<div className="collapse navbar-collapse" id="navbarTogglerDemo03">
-    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-        <li className="nav-item active">
+<div className="collapse navbar-collapse row" id="navbarTogglerDemo03">
+    <ul className="navbar-nav form-inline col">
+        <li className="nav-item active col">
             <a className="nav-link" href={`/`}>Home <span className="sr-only">(current)</span></a>
         </li>
-    </ul>
-    <form className="form-inline my-2 my-lg-1">
-        <input className="form-control my-lg-1" onChange={titleChanged} type="search" placeholder="Search Summoner Name.." aria-label="Search"/>
-        <a href={`/Search/${searchText}`} className="btn btn-outline-success my-2 my-sm-0">
+        <li className="nav-item col-8">
+    <form className="form-inline my-2 my-lg-8">
+        <input className="form-control form-control-lg col-8" onChange={titleChanged} type="text" placeholder="Search Summoner Name.." aria-label="Search"/>
+        <a href={`/Search/${searchText}`} className="btn form-control-lg btn-outline-success my-2 my-sm-0">
             Search
         </a>
-        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li className="nav-item active">
+    </form>
+        </li>
+            <li className="nav-item form-inline col">
                 <a className="nav-link" href={`/`}>Login <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item">
                 <a className="nav-link" href="#">Sign Up</a>
             </li>
-        </ul>
-    </form>
+    </ul>
 </div>
 </nav>
 
